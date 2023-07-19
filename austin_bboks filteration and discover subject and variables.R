7096c399ee41ccd9e50f7f0e8681ce9435756f49
Mohamed Mostafa <82689834+Mhsm2012@users.noreply.github.com>
2023-07-19 06:45:49 +0300

    Update austin_bboks filteration and discover subject and variables.R

--- austin_bboks filteration and discover subject and variables.R
+++ austin_bboks filteration and discover subject and variables.R
@@ -6,7 +6,7 @@ library(stringr)
 austen_books()
 #Get the Jane Austen books from the package janeaustenr and do some cleaning.
 original_books <- austen_books() %>% group_by(book) %>% mutate(linenumber=row_number(),
-                                                               chapter = cumsum(str_detect(text,regex("^chapter [\\divxlc]",ignore_case =TRUE))))%>% ungroup()
+  chapter = cumsum(str_detect(text,regex("^chapter [\\divxlc]",ignore_case =TRUE))))%>% ungroup()
 # check original_books
 original_books
 3 to know dimensions of the datasets
